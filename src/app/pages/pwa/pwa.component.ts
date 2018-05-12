1a4e2b1e2b52b87ceaa3898c20a04221a5cfe3a3
Migsar Navarro <migsar.navarro@gmail.com>
2018-05-12 06:19:46 -0500

    App and My Flight Layout

--- src/app/pages/pwa/pwa.component.ts
+++ src/app/pages/pwa/pwa.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Flight } from '../../commons/flight';
+import { FlightsService } from '../services/flights.service';
+
 @Component({
   selector: 'app-pwa',
   templateUrl: './pwa.component.html',
   styleUrls: ['./pwa.component.scss']
 })
 export class PwaComponent implements OnInit {
+  arrivals: Flight[];
+  departures: Flight[];
 
-  constructor() { }
+  constructor(fligths: FlightsService) {
+    this.arrivals = fligths.getArrivals();
+    this.departures = fligths.getDepartures();
+  }
 
   ngOnInit() {
   }
