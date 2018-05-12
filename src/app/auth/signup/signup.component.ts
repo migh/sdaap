e6eb91f2b519f61d3ec3ae593d831301bf8c8ae4
Alejandro Gutierrez <alex.gtz17@gmail.com>
2018-05-12 13:26:20 -0500

    generated signup and login components

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email]
+      }),
+      password: new FormControl('', { validators: [Validators.required] })
+    });
+  }
+
+}
