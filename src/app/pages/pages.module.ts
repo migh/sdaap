57c6c7738da074bea9a1c8d619fb64b8fc5464ad
Migsar Navarro <migsar.navarro@gmail.com>
2018-05-13 13:12:01 -0500

    Add routing module to pages

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClarityModule } from '@clr/angular';
+
+import { AppRoutingModule } from '../app-routing.module';
+import { CommonsModule } from '../commons/commons.module';
 import { BigscreenComponent } from './bigscreen/bigscreen.component';
 import { PwaComponent } from './pwa/pwa.component';
 import { MyFlightComponent } from './my-flight/my-flight.component';
-import { CommonsModule } from '../commons/commons.module';
-import { ClarityModule } from '@clr/angular';
 
 @NgModule({
   imports: [
     CommonModule,
     CommonsModule,
-    ClarityModule
+    ClarityModule,
+    AppRoutingModule
   ],
   declarations: [BigscreenComponent, PwaComponent, MyFlightComponent]
 })
